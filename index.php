074ce13bebc685b0ff9e0831b433d06844edcc24
suful <kubarywka@gmail.com>
2016-05-11 12:35:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+﻿<?php
+session_start();
+	// Turn off all error reporting
+	error_reporting(0);
+	
+	if(($_SESSION['logged'])==true) {
+		echo "zalogowany? TAK !!!";
+	}
+	else {
+		echo "zalogowany? NIE !!!";
+	}
+
+	
+	if(isset($_GET['task'])){
+
+	if($_GET['task']=='categories')
+	{
+		include 'controller/categories.php';
+		$ob = new CategoriesController();
+		$ob->$_GET['action']();
+	} 
+	else if($_GET['task']=='articles') 
+	{
+		include 'controller/articles.php';
+		$ob = new ArticlesController();
+		$ob->$_GET['action']();
+	} 
+	else if($_GET['task']=='galeria') 
+	{
+		include 'controller/galeria.php';
+		$ob = new GaleriaController();
+		$ob->$_GET['action']();
+	} 
+	else if($_GET['task']=='rejestracja') 
+	{
+		include 'controller/rejestracja.php';
+		$ob = new RejestracjaController();
+		$ob->$_GET['action']();
+	} 
+	else if($_GET['task']=='logowanie') 
+	{
+		include 'controller/logowanie.php';
+		$ob = new LogowanieController();
+		$ob->$_GET['action']();
+	} 
+	else if($_GET['task']=='wyloguj') 
+	{
+		$_SESSION['logged'] = false;
+		header('Location:index.php');
+		session_destroy();
+		
+	}
+	
+	}
+	else 
+	{
+		include 'controller/galeria.php';
+		$ob = new GaleriaController();
+		$ob->index();
+	}
+
+?>
